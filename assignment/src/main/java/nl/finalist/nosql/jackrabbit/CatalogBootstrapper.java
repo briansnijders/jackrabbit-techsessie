32a1309ffd1bdfa82ff49dd60021ce601a38dc14
bsnijders <brian@finalist.nl>
2012-11-06 19:43:19 +0100

    Pushing modified bootstrap

--- assignment/src/main/java/nl/finalist/nosql/jackrabbit/CatalogBootstrapper.java
+++ assignment/src/main/java/nl/finalist/nosql/jackrabbit/CatalogBootstrapper.java
@@ -8,50 +8,54 @@ import nl.finalist.nosql.jackrabbit.util.RepositoryUtil;
 
 public class CatalogBootstrapper {
 
-public static void main(String[] args) throws Exception {
-Session session = RepositoryUtil.getSession();
+	public static void main(String[] args) throws Exception {
+		Session session = RepositoryUtil.getSession();
 
-try {
-// Get hold of the root node of this session's workspace
-Node rootNode = session.getRootNode();
+		try {
+			// Get hold of the root node of this session's workspace
+			Node rootNode = session.getRootNode();
 
-// Create a node under which the cars are stored. This is a direct child
-// of the root node. The root node has absolute path "/", the cars node
-// has relative path "cars", thus the absolute path of the cars node will be
-// "/cars"
-Node carsNode = rootNode.addNode("cars");
+			// Create a node under which the cars are stored. This is a direct
+			// child
+			// of the root node. The root node has absolute path "/", the cars
+			// node
+			// has relative path "cars", thus the absolute path of the cars node
+			// will be
+			// "/cars"
+			Node carsNode = rootNode.addNode("cars");
 
-// Bootstrap the Mercedes
-Node mercedes = loadMercedes(carsNode, session);
+			// Bootstrap the Mercedes
+			Node mercedes = loadMercedes(carsNode, session);
 
-// Bootstrap the Ferrari
-Node ferrari = loadFerrari(carsNode, session);
+			// Bootstrap the Ferrari
+			Node ferrari = loadFerrari(carsNode, session);
 
-// Ask JackRabbit to save the session, thus committing and persisting changes
-session.save();
+			// Ask JackRabbit to save the session, thus committing and
+			// persisting changes
+			session.save();
 
-// Dump repository contents by recursively walking the hierarchy
-RepositoryUtil.dump(rootNode);
-} finally {
-session.logout();
-}
-}
+			// Dump repository contents by recursively walking the hierarchy
+			RepositoryUtil.dump(rootNode);
+		} finally {
+			session.logout();
+		}
+	}
 
-private static Node loadMercedes(Node parentNode, Session session) throws RepositoryException {
-//TODO
-// Fix the implementation which makes the tests pass!
-// Create a Mercedes and return it. Returns null for now.
-Node mercedes = null;
+	private static Node loadMercedes(Node parentNode, Session session) throws RepositoryException {
+		// TODO
+		// Fix the implementation which makes the tests pass!
+		// Create a Mercedes and return it. Returns null for now.
+		Node mercedes = null;
 
-return mercedes;
-}
+		return mercedes;
+	}
 
-private static Node loadFerrari(Node parentNode, Session session) throws RepositoryException {
-//TODO
-// Fix the implementation which makes the tests pass!
-// Create a Ferrari and return it. Returns null for now.
-Node ferrari = null;
+	private static Node loadFerrari(Node parentNode, Session session) throws RepositoryException {
+		// TODO
+		// Fix the implementation which makes the tests pass!
+		// Create a Ferrari and return it. Returns null for now.
+		Node ferrari = null;
 
-return ferrari;
-}
+		return ferrari;
+	}
 }
